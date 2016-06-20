c7378115be21639ddb3a20a01c47e027ac920c88
Jens Kwasniok <Jens@Jens-Hac.local>
2016-06-20 20:06:38 +0200

    initial commit

---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+//
+//  main.cpp
+//  Sudoku Solver
+//
+//  Created by Jens Kwasniok on 20.06.16.
+//  Copyright © 2016 Jens Kwasniok. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	
+}
