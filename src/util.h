1003153a24e8ada013dc23163a741e44b3d30e96
Kwasniok <JensKwasniok@web.de>
2016-08-12 19:53:43 +0200

    added comments in util.h

--- src/util.h
+++ src/util.h
@@ -13,15 +13,25 @@
 #include <utility>
 #include <vector>
 
+//! @param c a container
+//! @param v value to search for
+//! @return true if value is in container
 template<class T, class V>
 inline bool has_value(T& c, const V& v) {
 	return std::find(c.begin(), c.end(), v) != c.end();
 }
 
+//! Removes (first occurence of) a value from the std::vector and returns true if value was in vector.
+//! his might change the order of the std::vector.
+//! @param c std::vector of any type
+//! @param v value to be removed from std::vector
+//! @return true if values was in vecotor
 template<class V>
 inline bool removed(std::vector<V>& c, const V& v) {
+	// search for first appearence of v in c
 	for (auto it = c.begin(); it != c.end(); ++it) {
 		if (v == *it) {
+			// swap value to back position and remove it
 			iter_swap(it, c.rbegin());
 			c.pop_back();
 			return true;;
@@ -30,6 +40,10 @@ inline bool removed(std::vector<V>& c, const V& v) {
 	return false;
 }
 
+//! Removes (first occurence of) a value from the std::vector.
+//! his might change the order of the std::vector.
+//! @param c std::vector of any type
+//! @param v value to be removed from std::vector
 template<class V>
 inline void remove(std::vector<V>& c, const V& v) {
 	removed(c, v);
